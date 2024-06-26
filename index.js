5ab01a6e800376defe0e37a6f22975cdee249d42
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 19:59:09 +0530

    delete specific joke

--- index.js
+++ index.js
@@ -70,6 +70,19 @@ app.patch("/jokes/:id", (req, res) => {
 })
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  }
+  else {
+    res
+      .status(404)
+      .json({ error: `joke with id: ${id} not found, no jokes were deleted` });
+  }
+});
 
 //8. DELETE All jokes
 
