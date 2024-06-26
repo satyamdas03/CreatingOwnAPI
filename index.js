34d15deef3e91baa82c2441aa22a7e1e78b79547
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 18:46:48 +0530

    getting specific joke

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * jokes.length);
   res.json(jokes[randomIndex]);
-})
+});
 
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
 
