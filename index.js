2fd93bde1f013d48a9ba9f31c87fadd898a6b9c7
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-26 18:57:18 +0530

    getting jokes based on filter

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.get("/jokes/:id", (req, res) => {
 });
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+})
 
 //4. POST a new joke
 
